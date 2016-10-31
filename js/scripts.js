725268e45aa2a447a75a004a109df77f52827de0
Loren Glenn and Rafael Perez <loren@>
2016-10-31 14:54:55 -0700

    finish basic to do list functionality including removing items

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-function Chores (title){
+function Chores (title, name, time, notes){
   this.title = title;
+  this.name = name;
+  this.time = time;
+  this.notes = notes;
+}
+
+Chores.prototype.removeChore = function(){
+  return this.title.strike();
 }
 
 
@@ -8,17 +15,37 @@ $(document).ready(function (){
 
   $("#input").submit(function (event){
     event.preventDefault();
-    var chore = $("#chore").val();
+    var title = $("#title").val();
+    var name = $("#name").val();
+    var time = $("#time").val();
+    var notes = $("#notes").val();
 
 
-    var newChore = new Chores(chore);
-    $("#chores").append("<li>" +newChore.title+ "</li>");
+    var newChore = new Chores(title, name, time, notes);
+
+    var details = "<div class='details'>" +"<p>Name: "+ newChore.name + "</p>"
+    +"<p>Time: "+ newChore.time + "</p>"+"<p>Notes: "+ newChore.notes + "</p><button type='button' class='finished btn btn-danger'>Finished Chore</button></div>";
+
+    $("#chores").append("<li><span class='choreTitle'>" + newChore.title + "</span>" + details +"</li>" );
+    $("li").last().click(function (){
+      $("div.details",this).slideToggle();
+
+    });
+
+    $(".finished").last().click(function(){
+      var newTitle = newChore.removeChore();
+      $(this).parents("li").find("span").empty().append(newTitle);
+
+    })
 
 
 
 
 
-
+    $("#title").val("");
+    $("#name").val("");
+    $("#time").val("");
+    $("#notes").val("");
   })
 
 });
