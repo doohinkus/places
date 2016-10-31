8952cb9906f46bf1ce37ba220b28343a65543f4a
Loren Glenn and Rafael Perez <rafael@>
2016-10-31 11:52:15 -0700

    add initial files with barebones functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,21 @@
-function Places (locale, landmarks, timeOfYear, notes, image){
-  this.locale = locale;
-  this.landmarks = landmarks;
-  this.timeOfYear = timeOfYear;
-  this.notes = notes;
-  this.image = image;
+function Chores (title){
+  this.title = title;
 }
+
 
 $(document).ready(function (){
 
+
   $("#input").submit(function (event){
     event.preventDefault();
-    var locale = $("#locale").val();
-    var landmarks = $("#landmarks").val();
-    var timeOfYear = $("#timeOfYear").val();
-    var notes = $("#notes").val();
-    var image = $("#image").val();
-
-    var newPlace = new Places(locale, landmarks, timeOfYear, notes, image);
-    $("#places").append("<li class = 'place'>" + newPlace.locale + "</li>");
+    var chore = $("#chore").val();
 
 
-    $('li').last().click(function(){
-      $("#output").empty().append("<p>Locale: " + newPlace.locale + "</p> <p>Landmarks: " + newPlace.landmarks + "</p><p>Time of year: " + newPlace.timeOfYear + "</p><p>Notes: " + newPlace.notes + "</p><p>Image: " + newPlace.image + "</p>");
-    });
+    var newChore = new Chores(chore);
+    $("#chores").append("<li>" +newChore.title+ "</li>");
+
+
+
 
 
 
